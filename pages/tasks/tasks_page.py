1b4a4f6afb685261f924c061bb01e1e589465026
Pixxxxta <krapachev@gmail.com>
2024-09-13 14:50:26 +0300

    Изменение

--- pages/tasks/tasks_page.py
+++ pages/tasks/tasks_page.py
@@ -37,7 +37,7 @@ class TasksPage:
     def click_med_book_btn(self):
         self._helpers.swipe_down()
         time.sleep(2)
-        self._helpers.wait_and_click('(//android.widget.Button[@text="Подробнее"])[2]')
+        self._helpers.wait_and_click('//android.widget.Button[@text="Подробнее"]')
 
     def check_current_url(self, current_url):
         """Проверяет, что страница с нужным url загружена"""
